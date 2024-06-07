4b61d653a5f0c3f416c6d3d9fc80636e52517454
shahin2525 <shahin451999@gmail.com>
2024-06-07 12:45:45 -0700

    add total price

--- js/summer-sale.js
+++ js/summer-sale.js
@@ -1,5 +1,9 @@
 document.getElementById("k-card-1").addEventListener("click", () => {
   const accessoriesPrice1 = getText("accessories-price-1");
+  const totalPrice = getText("total-price");
+  const newPrice = accessoriesPrice1 + totalPrice + 5;
+
+  setTextField("total-price", newPrice);
 });
 
 const getText = (textId) => {
@@ -7,4 +11,11 @@ const getText = (textId) => {
   const textFieldString = textField.innerText;
   const textFieldNumber = parseFloat(textFieldString);
   return textFieldNumber;
+};
+
+const setTextField = (textId, total) => {
+  const textField = document.getElementById(textId);
+
+  textField.innerText = total;
+  return textField;
 };
